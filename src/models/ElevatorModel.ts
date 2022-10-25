34ee83eac41380e9f5e4f48030d5a258015cd4ce
patrykxly <psokolinski01@gmail.com>
2022-10-25 20:00:31 +0200

    Fix bux on elevator view change

--- src/models/ElevatorModel.ts
+++ src/models/ElevatorModel.ts
@@ -1,8 +1,10 @@
 import { Direction } from "../enums/DirectionEnum";
+import { WaitingPerson } from "./WaitingPersonModel";
 
 export interface Elevator {
   Id: number;
   currentFloor: number;
   targetFloor: number;
   direction: Direction;
+  waitingPeople: Array<WaitingPerson>;
 }
